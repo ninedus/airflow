9a60114a3ad92734e755e2e4207083e2967afcb4
ninedus <ninedus@gmail.com>
2024-02-03 14:36:01 +0900

    shell update

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -1,2 +1,10 @@
 FRUIT=$1
-echo "You selected a fruit."
+if [ $FRUTE == APPLE ]; then
+	echo "You selected Apple"
+elif [ $FRUTE == ORANGE ]; then
+	echo "You selected Orange"
+elif [ $FRUTE == GRAPE ]; then
+	echo "You selected Grape"
+else
+	echo "You selected other fruit."
+fi
